89adfc7f36527285ad7358ae69d2e31f49a19c4f
RaviKumarNerusu <2300033344cse@gmail.com>
2025-07-29 11:56:19 +0530

    Version 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,7 +7,8 @@ function App() {
 
   return (
     <>
-      <h1>Dundi weds Vyshu </h1>
+      <h1>Dundi superstar </h1>
+      <h2>Utham</h2>
     </>
   )
 }
